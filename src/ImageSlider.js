c9ed61d3d533b062f32d5fbf7a18371ba5cd862f
rin1782 <colaerin@yahoo.com>
2021-05-28 05:25:00 -0400

    Done.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,16 @@
-// your ImageSlider code here!
+import React from 'react';
+
+class ImageSlider extends React.Component {
+
+    state = { currentSlideIndex: 0 }
+
+    render() {
+        return(
+            <div className="imgSlider">
+                <h1>I am on slide {this.state.currentSlideIndex}</h1>
+            </div>
+        )
+    }
+}
+
+export default ImageSlider;
